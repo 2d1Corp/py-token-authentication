ced28ddcc88cca145ec77e8834dd7c8cb1238c28
Oleksandr <vhsb721@email.com>
2026-09-21 18:12:17 +0200

    Remove redundant authentication settings

--- user/views.py
+++ user/views.py
@@ -1,7 +1,6 @@
 from rest_framework import generics
-from rest_framework.authentication import TokenAuthentication
 from rest_framework.authtoken.views import ObtainAuthToken
-from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.permissions import AllowAny
 from rest_framework.settings import api_settings
 
 from user.serializers import UserSerializer
@@ -18,8 +17,6 @@ class CreateTokenView(ObtainAuthToken):
 
 class ManageUserView(generics.RetrieveUpdateAPIView):
     serializer_class = UserSerializer
-    authentication_classes = (TokenAuthentication,)
-    permission_classes = (IsAuthenticated,)
 
     def get_object(self):
         return self.request.user
